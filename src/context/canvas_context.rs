46b2c31ade6c61834b79c34abaac6938e834f41b
alngo <alxengo.pro@gmail.com>
2023-12-12 19:41:11 +0100

    Implement context

---
+++ src/context/canvas_context.rs
@@ -0,0 +1,63 @@
+use crate::context::{Context, Coordinate};
+use wasm_bindgen::prelude::{JsCast, JsValue};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+
+pub struct CanvasContext {
+    pub canvas: HtmlCanvasElement,
+    context: CanvasRenderingContext2d,
+}
+
+impl CanvasContext {
+    pub fn new(width: u32, height: u32) -> Result<CanvasContext, JsValue> {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document
+            .create_element("canvas")?
+            .dyn_into::<HtmlCanvasElement>()?;
+        canvas.set_width(width);
+        canvas.set_height(height);
+        let context = canvas
+            .get_context("2d")?
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()?;
+        Ok(CanvasContext { canvas, context })
+    }
+}
+
+impl Context for CanvasContext {
+    fn draw_pixel(&self, coord: Coordinate, color: &str) -> Result<(), JsValue> {
+        self.context.set_fill_style(&JsValue::from_str(color));
+        self.context
+            .fill_rect(coord.0 as f64, coord.1 as f64, 1.0, 1.0);
+        Ok(())
+    }
+
+    fn draw_line(&self, start: Coordinate, end: Coordinate, color: &str) -> Result<(), JsValue> {
+        self.context.set_stroke_style(&JsValue::from_str(color));
+        self.context.begin_path();
+        self.context.move_to(start.0 as f64, start.1 as f64);
+        self.context.line_to(end.0 as f64, end.1 as f64);
+        self.context.stroke();
+        Ok(())
+    }
+
+    fn draw_rect(&self, start: Coordinate, end: Coordinate, color: &str) -> Result<(), JsValue> {
+        self.context.set_fill_style(&JsValue::from_str(color));
+        self.context
+            .fill_rect(start.0 as f64, start.1 as f64, end.0 as f64, end.1 as f64);
+        Ok(())
+    }
+
+    fn draw_circle(&self, center: Coordinate, radius: u32, color: &str) -> Result<(), JsValue> {
+        self.context.set_fill_style(&JsValue::from_str(color));
+        self.context.begin_path();
+        self.context.arc(
+            center.0 as f64,
+            center.1 as f64,
+            radius as f64,
+            0.0,
+            2.0 * std::f64::consts::PI,
+        )?;
+        self.context.fill();
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,66 +4,10 @@ use web_sys::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-type Coordinate = (u32, u32);
-
-struct SvgContext {
-    svg: SvgElement,
-}
-
-impl SvgContext {
-    fn new(width: u32, height: u32) -> Result<SvgContext, JsValue> {
-        let document = web_sys::window().unwrap().document().unwrap();
-        let svg = document
-            .create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?
-            .dyn_into::<SvgElement>()?;
-        svg.set_attribute("width", &width.to_string())?;
-        svg.set_attribute("height", &height.to_string())?;
-        svg.set_attribute("viewBox", &format!("0 0 {} {}", width, height))?;
-        Ok(SvgContext { svg })
-    }
-
-    fn draw_pixel(&self, coord: Coordinate, color: &str) -> Result<(), JsValue> {
-        let document = web_sys::window().unwrap().document().unwrap();
-        let rect = document
-            .create_element_ns(Some("http://www.w3.org/2000/svg"), "rect")?
-            .dyn_into::<SvgRectElement>()?;
-        rect.set_attribute("x", &coord.0.to_string())?;
-        rect.set_attribute("y", &coord.1.to_string())?;
-        rect.set_attribute("width", "1")?;
-        rect.set_attribute("height", "1")?;
-        rect.set_attribute("fill", color)?;
-        self.svg.append_child(&rect)?;
-        Ok(())
-    }
-}
-
-struct CanvasContext {
-    canvas: HtmlCanvasElement,
-    context: CanvasRenderingContext2d,
-}
-
-impl CanvasContext {
-    fn new(width: u32, height: u32) -> Result<CanvasContext, JsValue> {
-        let document = web_sys::window().unwrap().document().unwrap();
-        let canvas = document
-            .create_element("canvas")?
-            .dyn_into::<HtmlCanvasElement>()?;
-        canvas.set_width(width);
-        canvas.set_height(height);
-        let context = canvas
-            .get_context("2d")?
-            .unwrap()
-            .dyn_into::<CanvasRenderingContext2d>()?;
-        Ok(CanvasContext { canvas, context })
-    }
-
-    fn draw_pixel(&self, coord: Coordinate, color: &str) -> Result<(), JsValue> {
-        self.context.set_fill_style(&JsValue::from_str(color));
-        self.context
-            .fill_rect(coord.0 as f64, coord.1 as f64, 1.0, 1.0);
-        Ok(())
-    }
-}
+mod context;
+use context::canvas_context::CanvasContext;
+use context::svg_context::SvgContext;
+use context::Context;
 
 #[wasm_bindgen]
 pub struct App {
@@ -84,11 +28,17 @@ impl App {
     pub fn run(&self) -> Result<(), JsValue> {
         self.init();
         let svg_context = SvgContext::new(100, 100)?;
-        svg_context.draw_pixel((50, 50), "red")?;
+        // svg_context.draw_pixel((50, 50), "red")?;
+        // svg_context.draw_line((0, 0), (100, 100), "red")?;
+        // svg_context.draw_rect((0, 0), (100, 100), "red")?;
+        svg_context.draw_circle((50, 50), 50, "red")?;
         self.root.append_child(&svg_context.svg)?;
 
         let canvas_context = CanvasContext::new(100, 100)?;
-        canvas_context.draw_pixel((50, 50), "blue")?;
+        // canvas_context.draw_pixel((50, 50), "blue")?;
+        // canvas_context.draw_line((0, 0), (100, 100), "blue")?;
+        // canvas_context.draw_rect((0, 0), (100, 100), "blue")?;
+        canvas_context.draw_circle((50, 50), 50, "blue")?;
         self.root.append_child(&canvas_context.canvas)?;
 
         console::log_1(&JsValue::from_str("App is running!"));
