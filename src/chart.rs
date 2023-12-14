674c72b745763c888991a5dd1c30dc00119cc054
alngo <alxengo.pro@gmail.com>
2023-12-14 22:08:20 +0100

    Reflexion

--- src/chart.rs
+++ src/chart.rs
@@ -9,10 +9,10 @@ pub enum ChartKind {
 
 #[derive(PartialEq, Debug)]
 struct Ohlc {
-    pub open: f32,
-    pub high: f32,
-    pub low: f32,
-    pub close: f32,
+    pub(crate) open: f32,
+    pub(crate) high: f32,
+    pub(crate) low: f32,
+    pub(crate) close: f32,
 }
 
 // Offset is a tuple of (x, y) coordinates
@@ -38,9 +38,46 @@ impl Ohlc {
 }
 
 pub struct Chart {
-    kind: ChartKind,
     window: Window,
     data: Vec<Ohlc>,
+    kind: ChartKind,
+}
+
+pub trait Graphic {
+    fn draw(&self);
+}
+
+struct Rectangle {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+}
+
+impl Graphic for Rectangle {
+    fn draw(&self) {
+        todo!()
+    }
+}
+
+struct Line {
+    x1: f32,
+    y1: f32,
+    x2: f32,
+    y2: f32,
+}
+
+impl Graphic for Line {
+    fn draw(&self) {
+        todo!()
+    }
+}
+
+// Tmp layer to test
+pub trait Layer {
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
+    fn draw(&self, graphics: Vec<Box<dyn Graphic>>);
 }
 
 impl Chart {
@@ -56,11 +93,18 @@ impl Chart {
         }
     }
 
+    // Chart should have composable Layer
+    // Chart track reference to layers
+    // Layers are responsible to draw on the chart
+    // One mandatory layer is the ohlc layer wich display bars, candles or lines
+    // Other layers could be added to display indicators, etc.
+
     pub fn set_kind(&mut self, kind: ChartKind) {
         self.kind = kind;
     }
 
     pub fn set_window(&mut self, width: f32, height: f32, offset: Offset) {
+        // TODO: check if offset is valid
         self.window = Window {
             width,
             height,
@@ -90,6 +134,58 @@ impl Chart {
         let x = (coordinate.0 - self.window.offset.0) / x_scale;
         let y = (coordinate.1 - self.window.offset.1) / y_scale;
         (x, y)
+    }
+
+    fn get_candles(&self) -> &[Ohlc] {
+        if self.data.len() < self.window.offset.0 as usize {
+            return &[]
+        }
+        let end = self.window.offset.0 as usize + self.window.width as usize;
+        if end > self.data.len() {
+            return &self.data[self.window.offset.0 as usize..]
+        }
+        &self.data[self.window.offset.0 as usize..end]
+    }
+
+    // /?\ Should I consider draw_on with the following signature:
+    // pub fn draw_on(&self, window: &Window, layer: &impl Layer)
+    // or
+    // pub fn draw_window_on_layer(&self, window: &Window, layer: &impl Layer)
+    // to allow the layer to choose its window?
+    // removing the window_to_viewport responsability from the chart
+    // window_to_viewport can move to utility? Anti-pattern?
+    // Chart can cache "Graphics" instead of "Ohlc" to avoid the conversion
+    // and avoid previous calculation
+    // How to share the World/Chart?
+    // How to draw the grid?
+    // How to draw the indicator?
+    pub fn draw_on(&self, layer: &impl Layer) {
+        let candles: &[Ohlc] = self.get_candles();
+        let (width, height) = (layer.width(), layer.height());
+        match self.kind {
+            ChartKind::Candle => {
+                let mut graphics: Vec<Box<dyn Graphic>> = Vec::new();
+                for (index, candle) in candles.iter().enumerate() {
+                    let x = index as f32;
+                    let y = candle.open;
+                    let width = 1.0;
+                    let height = candle.close - candle.open;
+                    let rectangle = Rectangle { x, y, width, height };
+                    let line = Line { x1: x + 0.5, y1: candle.low, x2: x + 0.5, y2: candle.high };
+
+                    graphics.push(Box::new(rectangle));
+                    graphics.push(Box::new(line));
+                }
+                layer.draw(graphics);
+            }
+            ChartKind::Line => {
+                todo!()
+            }
+            ChartKind::Bar => {
+                todo!()
+            }
+
+        }
     }
 }
 
@@ -168,38 +264,37 @@ mod chart_tests {
     }
 
     #[test]
-    fn test_window_to_viewport_1_1() {
+    fn test_window_to_viewport_coordinates() {
         let mut chart = Chart::new();
         chart.set_window(100.0, 2.0, (0.0, 0.0));
         let viewport = (100, 100);
         let coordinate = (1.0, 1.0);
         assert_eq!(chart.window_to_viewport(coordinate, viewport), (1.0, 50.0));
-    }
-
-    #[test]
-    fn test_window_to_viewport_2_2() {
-        let mut chart = Chart::new();
-        chart.set_window(100.0, 2.0, (0.0, 0.0));
-        let viewport = (100, 100);
+        let coordinate = (1.0, 1.5);
+        assert_eq!(chart.window_to_viewport(coordinate, viewport), (1.0, 75.0));
         let coordinate = (2.0, 2.0);
         assert_eq!(chart.window_to_viewport(coordinate, viewport), (2.0, 100.0));
     }
 
     #[test]
-    fn test_window_to_viewport_multiple() {
-        let mut chart = Chart::new();
-        chart.set_window(100.0, 2.0, (0.0, 0.0));
-        let viewport = (100, 100);
-        let coordinate = (1.0, 1.5);
-        assert_eq!(chart.window_to_viewport(coordinate, viewport), (1.0, 75.0));
-    }
-
-    #[test]
-    fn test_window_to_viewport_with_offset() {
+    fn test_window_to_viewport_coordinates_with_offset() {
         let mut chart = Chart::new();
         chart.set_window(100.0, 2.0, (1.0, 1.0));
         let viewport = (100, 100);
+        let coordinate = (0.0, 0.0);
+        assert_eq!(
+            chart.window_to_viewport(coordinate, viewport),
+            (-1.0, -50.0)
+        );
+        let coordinate = (1.0, 1.0);
+        assert_eq!(chart.window_to_viewport(coordinate, viewport), (0.0, 0.0));
+        let coordinate = (1.0, 1.5);
+        assert_eq!(chart.window_to_viewport(coordinate, viewport), (0.0, 25.0));
         let coordinate = (1.0, 2.0);
         assert_eq!(chart.window_to_viewport(coordinate, viewport), (0.0, 50.0));
+    }
+
+    #[test]
+    fn test_get_candles() {
     }
 }
