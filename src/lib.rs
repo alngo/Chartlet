e11024505f365d414aece022dc1f8cc1790e1695
Alngo <alxengo.pro@gmail.com>
2024-01-03 08:50:00 -0500

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,38 @@
-use std::collections::HashMap;
-
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use web_sys::*;
+
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-pub mod builder;
-pub mod chart;
-pub mod graphic;
 
+// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen]
-pub fn init() {
-    set_panic_hook();
-}
-
-#[derive(Default, Clone, Serialize, Deserialize)]
-struct Layer {
-    pub name: Box<str>,
-}
-
-#[derive(Default, Serialize, Deserialize)]
-struct Layers {
-    pub layers: HashMap<String, Layer>,
-}
-
-impl Layers {
-    pub fn default() -> Self {
-        let mut layers = HashMap::new();
-        layers.insert(
-            "layer1".to_string(),
-            Layer {
-                name: "layer1".into(),
-            },
-        );
-        layers.insert(
-            "layer2".to_string(),
-            Layer {
-                name: "layer2".into(),
-            },
-        );
-        let svg = web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")
-            .unwrap();
-        svg.set_attribute("width", "100%").unwrap();
-        svg.set_attribute("height", "100%").unwrap();
-        svg.set_attribute("viewBox", "0 0 100 100").unwrap();
-        layers.insert(
-            "layer3".to_string(),
-            Layer {
-                name: svg.outer_html().into(),
-            },
-        );
-        Self { layers }
-    }
-}
-
-#[wasm_bindgen]
-pub fn load_layers() -> JsValue {
-    let layers = Layers::default();
-    serde_wasm_bindgen::to_value(&layers).unwrap()
-}
-
-fn set_panic_hook() {
+pub fn run() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
+
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello from Rust!"));
+
+    body.append_child(&val)?;
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
 }
