aba578542ee30b5728ec6c41aa29aedc42b8b7e1
Alngo <alxengo.pro@gmail.com>
2024-01-11 08:28:03 +0100

    Simple svg context implementation

--- src/view.rs
+++ src/view.rs
@@ -15,6 +15,7 @@ pub enum Layer {
     Indicators,
     Objects,
     Orders,
+    Pointer,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
--- src/view/context/svg.rs
+++ src/view/context/svg.rs
@@ -1,8 +1,32 @@
+use wasm_bindgen::JsCast;
+use web_sys::{window, Document, SvgElement};
+
 use crate::view::Point;
 
 use super::Context;
 
-struct Svg;
+struct Svg {
+    document: Document,
+    svg: SvgElement,
+}
+
+impl Svg {
+    pub fn new(width: u32, height: u32) -> Svg {
+        let document = window().unwrap().document().unwrap();
+        let svg = document
+            .create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")
+            .unwrap()
+            .dyn_into::<SvgElement>()
+            .unwrap();
+        svg.set_attribute("width", &width.to_string()).unwrap();
+        svg.set_attribute("height", &height.to_string()).unwrap();
+        svg.set_attribute("viewBox", &format!("0 0 {} {}", width, height))
+            .unwrap();
+        Svg { document, svg }
+    }
+}
+
+// pub struct Point(pub f64, pub f64);
 
 impl Context for Svg {
     fn draw_pixel(&self, point: Point) -> Result<(), String> {
